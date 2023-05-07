592e58efcef5d7e95bbd2503f1997f2e136e44a8
Francisco Aguirre <franmaguirre@gmail.com>
2023-05-07 09:21:44 -0300

    Agregar archivos de proyecto.

---
+++ CustomersAPI/Controllers/CustomerController.cs
@@ -0,0 +1,53 @@
+﻿using CustomersAPI.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomersAPI.Controllers
+{
+
+
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+
+
+
+        //api/customer
+        [HttpGet]
+        public async Task<List<CustomerDto>> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<CustomerDto> GetCustomer(long id)
+
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<bool> DeleteCustomer(long id)
+
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<CustomerDto> CreateCustomer(CreateCustomerDto customer)
+
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public async Task<CustomerDto> UpdateCustomer(CustomerDto customer)
+
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ CustomersAPI/Dtos/CreateCustomerDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomersAPI.Dtos
+{
+    public class CreateCustomerDto
+    {
+        [Required (ErrorMessage = "El primer nombre debe estar especificado")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "El apellido tiene que estar especificado")]
+        public string LastName { get; set; }
+
+        
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+    }
+}
